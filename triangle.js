ac1257761ce8936a1a647db5c76eedf7214f70f4
Auberon Lopez <auberon.lopez@greenriver.edu>
2026-07-28 15:29:56 -0400

    Typechecking canvas when resizing

--- triangle.js
+++ triangle.js
@@ -214,10 +214,12 @@ async function main() {
     const observer = new ResizeObserver(entries => {
         for (const entry of entries) {
             const canvas = entry.target;
-            const width = entry.contentBoxSize[0].inlineSize;
-            const height = entry.contentBoxSize[0].blockSize;
-            canvas.width = Math.max(1, Math.min(width, device.limits.maxTextureDimension2D));
-            canvas.height = Math.max(1, Math.min(height, device.limits.maxTextureDimension2D));
+            if(canvas instanceof HTMLCanvasElement) {
+                const width = entry.contentBoxSize[0].inlineSize;
+                const height = entry.contentBoxSize[0].blockSize;
+                canvas.width = Math.max(1, Math.min(width, device.limits.maxTextureDimension2D));
+                canvas.height = Math.max(1, Math.min(height, device.limits.maxTextureDimension2D));
+            }
         }
     });
     observer.observe(canvas);
